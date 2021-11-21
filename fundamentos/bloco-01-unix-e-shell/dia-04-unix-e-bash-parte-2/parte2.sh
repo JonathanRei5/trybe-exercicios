df670d0ccd454585c6484a9b5157da2cbe457917
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-21 20:34:39 -0300

    Adicionado comentários nos exercícios parte1.sh e parte2.sh

--- fundamentos/bloco-01-unix-e-shell/dia-04-unix-e-bash-parte-2/parte2.sh
+++ fundamentos/bloco-01-unix-e-shell/dia-04-unix-e-bash-parte-2/parte2.sh
@@ -1,23 +1,28 @@
 esperar=2
 
+# Exercício 1.
 echo "Navegando até a pasta \"unix_tests\""
 cd unix_tests
 sleep $esperar
 
+# Exercício 2.
 echo -e "\nVendo as permissões dos arquivos\n"
 ls -l
 sleep $esperar
 
+# Exercício 3.
 echo -e "\nMudando a permissão do arquivo \"bunch_of_things.txt\" para que todos os usuários possam ter acesso à leitura e escrita\ne verificando com o comando \"ls -l\"\n"
 chmod a=rw bunch_of_things.txt
 ls -l
 sleep $esperar
 
+# Exercício 4.
 echo -e "\nTirando a permissão de escrita do arquivo \"bunch_of_things.txt\" para todos os usuários e verificando com o comando \"ls -l\"\n"
 chmod a-w bunch_of_things.txt
 ls -l
 sleep $esperar
 
+# Exercício 5.
 echo -e "\nVoltando para as permissões listadas inicialmente\n"
 chmod 644 bunch_of_things.txt
 ls -l
