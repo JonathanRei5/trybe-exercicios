531aa83d423baefe80636f3f21e6aaca4e803128
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-26 19:12:45 -0300

    exercicios/4.1 | Exercicio 2 feito

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio2.js
@@ -0,0 +1,9 @@
+const a = 10000;
+const b = 10000;
+
+if (a > b)
+    console.log(a + " é maior.");
+else if (b > a)
+    console.log(b + " é maior.");
+else
+    console.log("Os dois são iguais.");
