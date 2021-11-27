ae263df8548a87d8397b71ad5132e9a120a57598
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-27 03:12:04 -0300

    exercicios/4.1 | Exercicio 2 funçao adicionada

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio2.js
@@ -1,9 +1,11 @@
+function maiorNumero(a, b) {
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
 const a = 10;
 const b = 100;
 
-if (a > b)
-    console.log(a + " é maior.");
-else if (b > a)
-    console.log(b + " é maior.");
-else
-    console.log("Os dois são iguais.");
+console.log(maiorNumero(a, b));
