b48d904c480cde26f6a21caeb2a6042cd37bea76
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-27 02:53:25 -0300

    exercicios/4.1 | Exercicio 1 funçoes adicionadas

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio1.js
@@ -1,8 +1,24 @@
+function soma(a, b) {
+    return a + b;
+}
+function subtracao(a, b) {
+    return a - b;
+}
+function multiplicacao(a, b) {
+    return a * b;
+}
+function divisao(a, b) {
+    return a / b;
+}
+function modulo(a, b) {
+    return a % b;
+}
+
 const a = 5;
 const b = 2;
 
-console.log(a+" + "+b+" = "+(a+b));
-console.log(a+" - "+b+" = "+(a-b));
-console.log(a+" * "+b+" = "+(a*b));
-console.log(a+" / "+b+" = "+(a/b));
-console.log(a+" % "+b+" = "+(a%b));
+console.log(a + " + " + b + " = " + soma(a, b));
+console.log(a + " - " + b + " = " + subtracao(a, b));
+console.log(a + " * " + b + " = " + multiplicacao(a, b));
+console.log(a + " / " + b + " = " + divisao(a, b));
+console.log(a + " % " + b + " = " + modulo(a, b));
