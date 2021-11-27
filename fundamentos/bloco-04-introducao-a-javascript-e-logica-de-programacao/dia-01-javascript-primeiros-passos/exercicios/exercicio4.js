5390fe8ef557dd35b58951c14580cb9ff27aeb34
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-27 02:59:53 -0300

    exercicios/4.1 | Exercicio 4 funçao adicionada

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio4.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio4.js
@@ -1,8 +1,12 @@
+function verificarNumero(valor) {
+    if (valor > 0)
+        return "positive";
+    else if (valor < 0)
+        return "negative";
+    else
+        return "zero";
+}
+
 const valor = 1;
 
-if (valor > 0)
-    console.log("positive");
-else if (valor < 0)
-    console.log("negative");
-else
-    console.log("zero");
+console.log(verificarNumero(valor));
