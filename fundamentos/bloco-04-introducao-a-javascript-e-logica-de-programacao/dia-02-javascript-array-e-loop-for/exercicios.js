43dcaca40564c572fbcacd733f2052bc413c4595
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-29 18:19:17 -0300

    exercicios/4.2 | Exercício 3 feito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -2,6 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let linha = "------------------------------------------------------------";
 
 // Exercício 1
+console.log(linha);
 console.log("Exercício 1:");
 for (let number of numbers)
     console.log(number);
@@ -13,4 +14,13 @@ let soma = 0;
 for (let number of numbers)
     soma += number;
 console.log("Resultado da soma = " + soma);
+console.log(linha);
+
+// Exercício 3
+console.log("Exercício 3:");
+let media = 0;
+for (let number of numbers)
+    media += number;
+media /= numbers.length;
+console.log("Resultado da média = " + media);
 console.log(linha);
