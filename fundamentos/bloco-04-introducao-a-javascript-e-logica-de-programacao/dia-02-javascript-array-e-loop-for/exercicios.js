a1a40eb6b0bd53c470894049f08be586a0c4ac70
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-11-29 18:08:37 -0300

    exercicios/4.2 | Exercício 1 feito

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let linha = "------------------------------------------------------------";
+
+// Exercício 1
+console.log("Exercício 1:");
+for (let number of numbers)
+    console.log(number);
+console.log(linha);
