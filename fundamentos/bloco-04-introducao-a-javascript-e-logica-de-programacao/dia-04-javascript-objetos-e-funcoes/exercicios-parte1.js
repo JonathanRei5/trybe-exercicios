0747236d47afea8f4e2bc9fafb377b4bcb56362a
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-01 20:23:50 -0300

    exercicios/4.4 | Exercício 5 da parte 1 feito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicios-parte1.js
@@ -29,3 +29,27 @@ console.log("Exercício 4:\n");
 for (let key in info)
   console.log(info[key]);
 console.log(linha);
+
+// Exercício 5
+console.log("Exercício 5:\n");
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+for (let key in info) {
+  if (key === 'recorrente') {
+    if (info[key] === info2[key]) {
+      if (info['recorrente'] === 'Sim')
+        console.log("Ambos recorrentes");
+      else
+        console.log("Ambos não recorrentes");
+    }
+    else
+      console.log(info[key] + " e " + info2[key]);
+  }
+  else
+    console.log(info[key] + " e " + info2[key]);
+}
+console.log(linha);
