cdfb274bf7d349690bc2bf650adf66c1630ad8cc
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-03 13:49:34 -0300

    exercicios/4.4 | Exercício bônus 4 feito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/README.md
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/README.md
@@ -215,3 +215,40 @@ const basket = [
   'Banana', 'Pera', 'Abacate', 'Uva',
 ];
 ```
+
+Usando o objeto abaixo, faça os exercícios a seguir:
+
+```javascript
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+```
+
+4. Acesse as chaves nome, sobrenome, andar e apartamento do último morador do blocoDois e faça um console.log no seguinte formato: "O morador do bloco 2 de nome Zoey Brooks mora no 1° andar, apartamento 101".
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/bonus.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/bonus.js
@@ -104,3 +104,46 @@ mensagem = mensagem.substr(0, mensagem.length - 2);
 console.log(mensagem);
 
 console.log(linha);
+
+let moradores = {
+  blocoUm: [
+    {
+      nome: 'Luiza',
+      sobrenome: 'Guimarães',
+      andar: 10,
+      apartamento: 1005,
+    },
+    {
+      nome: 'William',
+      sobrenome: 'Albuquerque',
+      andar: 5,
+      apartamento: 502,
+    },
+  ],
+  blocoDois: [
+    {
+      nome: 'Murilo',
+      sobrenome: 'Ferraz',
+      andar: 8,
+      apartamento: 804,
+    },
+    {
+      nome: 'Zoey',
+      sobrenome: 'Brooks',
+      andar: 1,
+      apartamento: 101,
+    },
+  ],
+};
+
+// Exercício bônus 4
+console.log("Exercício bônus 4:\n");
+
+let nome = moradores.blocoDois[1].nome;
+let sobrenome = moradores.blocoDois[1].sobrenome;
+let andar = moradores.blocoDois[1].andar;
+let apartamento = moradores.blocoDois[1].apartamento;
+
+console.log('O morador do bloco 2 de nome ' + nome + ' ' + sobrenome + ' mora no ' + andar + 'º andar, apartamento ' + apartamento);
+
+console.log(linha);
