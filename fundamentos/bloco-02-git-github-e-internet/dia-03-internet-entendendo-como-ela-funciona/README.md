b39e20d5a00c7dc79e75d67a701640f62bc25d07
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-05 22:32:01 -0300

    exercicios/2.3 | Exercicio 2 feito

--- fundamentos/bloco-02-git-github-e-internet/dia-03-internet-entendendo-como-ela-funciona/README.md
+++ fundamentos/bloco-02-git-github-e-internet/dia-03-internet-entendendo-como-ela-funciona/README.md
@@ -1,3 +1,5 @@
 # Exercícios do site [jvns.ca](https://jvns.ca/blog/2019/08/27/curl-exercises/)
 
 1. Faça uma requisição para https://httpbin.org
+
+2. Faça uma requisição para https://httpbin.org/anything
--- fundamentos/bloco-02-git-github-e-internet/dia-03-internet-entendendo-como-ela-funciona/exercicios-curl.sh
+++ fundamentos/bloco-02-git-github-e-internet/dia-03-internet-entendendo-como-ela-funciona/exercicios-curl.sh
@@ -1,6 +1,11 @@
-esperar=2
+esperar=0
 
 # Exercício 1
 echo -e "Exercício 1:\n"
 curl https://httpbin.org/
+sleep $esperar
+
+# Exercício 2
+echo -e "\n\nExercício 2:\n"
+curl https://httpbin.org/anything
 sleep $esperar
