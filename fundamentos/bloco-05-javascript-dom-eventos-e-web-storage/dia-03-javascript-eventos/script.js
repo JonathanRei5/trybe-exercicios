7451f5d2be6c62616fcce07f83953aa37dc36dcd
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-07 21:44:46 -0300

    exercicios/5.3 | Exercício 1 feito

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/script.js
@@ -14,3 +14,26 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Exercício 1:
+
+function addDayOfMonth(days, month, holidays, fridays) {
+  for (let i = 0; i < days.length; i += 1) {
+    const day = days[i];
+    const dayOfMonth = document.createElement('li');
+    dayOfMonth.innerText = day;
+    dayOfMonth.classList.add('day');
+    if (holidays.indexOf(day) > -1) {
+      dayOfMonth.classList.add('holiday');
+    }
+    if (fridays.indexOf(day) > -1) {
+      dayOfMonth.classList.add('friday');
+    }
+    month.appendChild(dayOfMonth);
+  }
+}
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const holidays = [24, 25, 31];
+const fridays = [4, 11, 18, 25];
+const month = document.getElementById('days');
+addDayOfMonth(dezDaysList, month, holidays, fridays);
