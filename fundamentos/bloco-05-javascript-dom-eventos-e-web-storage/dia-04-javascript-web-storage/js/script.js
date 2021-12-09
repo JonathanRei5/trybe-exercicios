ddbfcbbb9ed4e811dfd580b7e20f570ea9725943
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-09 13:37:56 -0300

    exercicios/5.4 | Adicionada opcao de selecionar um botão do menu e fazer aparecer o input

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/js/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/js/script.js
@@ -2,6 +2,8 @@ window.onload = function () {
   // Obtendo os elementos
   const btn_hamburguer = document.getElementById('btn_hamburguer');
   const menu = document.querySelector('.menu');
+  const menu_opcao = document.getElementsByClassName('menu_opcao');
+  const input_conteiner = document.getElementById('input_conteiner');
 
   // Adiciona opção de abrir e fechar o menu
   btn_hamburguer.addEventListener('click', function () {
@@ -11,4 +13,16 @@ window.onload = function () {
       menu.classList.add('menu_abrir');
     }
   });
+
+  // Adiciona opção de selecionar botão no menu
+  function selecionarMenuOpcao(event) {
+    for (let i = 0; i < menu_opcao.length; i += 1) {
+      menu_opcao[i].classList.remove('menu_opcao_selecionada');
+    }
+    event.target.classList.add('menu_opcao_selecionada');
+    input_conteiner.style.display = 'block';
+  }
+  for (let i = 0; i < menu_opcao.length; i += 1) {
+    menu_opcao[i].addEventListener('click', selecionarMenuOpcao);
+  }
 }
