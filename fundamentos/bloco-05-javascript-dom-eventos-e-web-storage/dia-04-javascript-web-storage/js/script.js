80aaba620051b1ecbb837fd7fec5b2e91180fdae
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-09 13:47:52 -0300

    exercicios/5.4 | Adicionada opcao de fechar o input

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/js/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/js/script.js
@@ -4,6 +4,7 @@ window.onload = function () {
   const menu = document.querySelector('.menu');
   const menu_opcao = document.getElementsByClassName('menu_opcao');
   const input_conteiner = document.getElementById('input_conteiner');
+  const input_fechar = document.getElementById('input_fechar');
 
   // Adiciona opção de abrir e fechar o menu
   btn_hamburguer.addEventListener('click', function () {
@@ -14,7 +15,7 @@ window.onload = function () {
     }
   });
 
-  // Adiciona opção de selecionar botão no menu
+  // Adiciona opção de selecionar botão no menu e abrir o input
   function selecionarMenuOpcao(event) {
     for (let i = 0; i < menu_opcao.length; i += 1) {
       menu_opcao[i].classList.remove('menu_opcao_selecionada');
@@ -25,4 +26,11 @@ window.onload = function () {
   for (let i = 0; i < menu_opcao.length; i += 1) {
     menu_opcao[i].addEventListener('click', selecionarMenuOpcao);
   }
+
+  // Adiciona opção de fechar o input
+  input_fechar.addEventListener('click', function () {
+    const botaoSelecionado = document.querySelector('.menu_opcao.menu_opcao_selecionada');
+    botaoSelecionado.classList.remove('menu_opcao_selecionada');
+    input_conteiner.style.display = 'none';
+  });
 }
