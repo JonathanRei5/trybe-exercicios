c421197442d7318f331e1646fdc65cd026b30f37
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-05 13:45:00 -0300

    exercicios/6.1 | Estados adicionados pelo JavaScript

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/script/script.js
@@ -0,0 +1,42 @@
+// Adiciona os estados no elemento select
+function adicionarEstados() {
+  const estados = [
+    ['AC', 'Acre'],
+    ['AL', 'Alagoas'],
+    ['AP', 'Amapá'],
+    ['AM', 'Amazonas'],
+    ['BA', 'Bahia'],
+    ['CE', 'Ceará'],
+    ['ES', 'Espírito Santo'],
+    ['GO', 'Goiás'],
+    ['MA', 'Maranhão'],
+    ['MT', 'Mato Grosso'],
+    ['MS', 'Mato Grosso do Sul'],
+    ['MG', 'Minas Gerais'],
+    ['PA', 'Pará'],
+    ['PB', 'Paraíba'],
+    ['PR', 'Paraná'],
+    ['PE', 'Pernambuco'],
+    ['PI', 'Piauí'],
+    ['RJ', 'Rio de Janeiro'],
+    ['RN', 'Rio Grande do Norte'],
+    ['RS', 'Rio Grande do Sul'],
+    ['RO', 'Rondônia'],
+    ['RR', 'Roraima'],
+    ['SC', 'Santa Catarina'],
+    ['SP', 'São Paulo'],
+    ['SE', 'Sergipe'],
+    ['TO', 'Tocantins'],
+    ['DF', 'Distrito Federal']
+  ];
+
+  const selectEstado = document.getElementById('estado');
+  for (const estado of estados) {
+    const option = document.createElement('option');
+    option.value = estado[0];
+    option.textContent = estado[1];
+    selectEstado.append(option);
+  }
+}
+
+adicionarEstados();
