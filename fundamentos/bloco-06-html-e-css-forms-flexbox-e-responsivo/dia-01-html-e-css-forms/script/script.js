3837f8376778bb14a7f1c1a116ef928f7400e701
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-08 17:07:02 -0300

    exercicios/6.1 | Validações feitas via JavaScript

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script/script.js
@@ -76,6 +76,21 @@ function verificarRadios() {
   return false;
 }
 
+// Remove a mensagem de erro
+function removerMensagemDeErro(event) {
+  if (event.target !== BTN_SUBMIT) {
+    MENSAGEM_ERRO.style.display = 'none';
+    window.clearTimeout(idTimeout);
+    document.removeEventListener('click', removerMensagemDeErro);
+  }
+}
+
+// Destaca um campo inválido
+function destacarCampoInvalido(campo) {
+  campo.focus();
+  campo.scrollIntoView();
+}
+
 // Insere uma mensagem de erro abaixo do elemento
 let idTimeout;
 function inserirMensagemDeErro(elemento, mensagem, tempo) {
@@ -85,21 +100,14 @@ function inserirMensagemDeErro(elemento, mensagem, tempo) {
   MENSAGEM_ERRO.style.top = String(bottom).concat('px');
   MENSAGEM_ERRO_TEXTO.textContent = mensagem;
 
-  MENSAGEM_ERRO.scrollIntoView();
-  elemento.focus();
+  destacarCampoInvalido(elemento);
 
   window.clearTimeout(idTimeout);
   idTimeout = window.setTimeout(() => {
     MENSAGEM_ERRO.style.display = 'none';
   }, tempo);
-}
 
-// Remove a mensagem de erro
-function removerMensagemDeErro(event) {
-  if (event.target !== BTN_SUBMIT) {
-    MENSAGEM_ERRO.style.display = 'none';
-    window.clearTimeout(idTimeout);
-  }
+  document.addEventListener('click', removerMensagemDeErro);
 }
 
 // Verifica se os campos do formulário estão vazios
@@ -126,16 +134,138 @@ function verificarCamposVazios() {
   return true;
 }
 
+// Verifica se o e-mail está correto
+function verificarEmail() {
+  const subEmail = CAMPOS_FORMULARIO.IMPUT_EMAIL.value.split('@');
+
+  if (subEmail.length < 2) {
+    window.alert('Insira um "@" no e-mail.');
+    return false;
+  } else if (subEmail.length > 2) {
+    window.alert('Apenas um "@" é permitido no e-mail.');
+    return false;
+  } else {
+    if (stringVazia(subEmail[0])) {
+      window.alert('Insira uma parte antes do "@" no e-mail.');
+      return false;
+    } else if (stringVazia(subEmail[1])) {
+      window.alert('Insira uma parte depois do "@" no e-mail.');
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Verifica se o CPF está correto
+function verificarCPF() {
+  const cpf = CAMPOS_FORMULARIO.IMPUT_CPF.value;
+  if (/[^0-9]/g.test(cpf)) {
+    window.alert('Insira apenas números no CPF.');
+    return false;
+  }
+  if (cpf.length !== 11) {
+    window.alert('O CPF deve conter 11 dígitos.');
+    return false;
+  }
+  return true;
+}
+
+// Verifica o dia da data
+function verificarDiaData(dia) {
+  if (stringVazia(dia)) {
+    window.alert('Insira um dia.');
+    return false;
+  }
+  if (/[^0-9]/g.test(dia)) {
+    window.alert('Insira apenas números no dia.');
+    return false;
+  }
+  if (Number(dia) < 1 || Number(dia) > 31) {
+    window.alert('O dia está incorreto.');
+    return false;
+  }
+
+  return true;
+}
+
+// Verifica o mes da data
+function verificarMesData(mes) {
+  if (stringVazia(mes)) {
+    window.alert('Insira um mês.');
+    return false;
+  }
+  if (/[^0-9]/g.test(mes)) {
+    window.alert('Insira apenas números no mês.');
+    return false;
+  }
+  if (Number(mes) < 1 || Number(mes) > 12) {
+    window.alert('O mês está incorreto.');
+    return false;
+  }
+
+  return true;
+}
+
+// Verifica o ano da data
+function verificarAnoData(ano) {
+  if (stringVazia(ano)) {
+    window.alert('Insira um ano.');
+    return false;
+  }
+  if (/[^0-9]/g.test(ano)) {
+    window.alert('Insira apenas números no ano.');
+    return false;
+  }
+
+  return true;
+}
+
+// Verifica se a data está correta
+function verificarData() {
+  const subData = CAMPOS_FORMULARIO.IMPUT_INICIO.value.split('/');
+
+  if (subData.length !== 3) {
+    window.alert('Insira uma data no formato DD/MM/AAAA.');
+    return false;
+  }
+
+  if (!verificarDiaData(subData[0])) {
+    return false;
+  }
+  if (!verificarMesData(subData[1])) {
+    return false;
+  }
+  if (!verificarAnoData(subData[2])) {
+    return false;
+  }
+
+  return true;
+}
+
 // Faz as verificações dos campos do formulário
 function verificarInputs(event) {
   event.preventDefault();
-  verificarCamposVazios();
+  if (!verificarCamposVazios()) {
+    return;
+  }
+  if (!verificarEmail()) {
+    destacarCampoInvalido(CAMPOS_FORMULARIO.IMPUT_EMAIL);
+    return;
+  }
+  if (!verificarCPF()) {
+    destacarCampoInvalido(CAMPOS_FORMULARIO.IMPUT_CPF);
+    return;
+  }
+  if (!verificarData()) {
+    destacarCampoInvalido(CAMPOS_FORMULARIO.IMPUT_INICIO);
+    return;
+  }
 }
 
 // Adiciona ouvintes nos elementos
 function adicionarOuvintes() {
   BTN_SUBMIT.addEventListener('click', verificarInputs);
-  document.addEventListener('click', removerMensagemDeErro);
 }
 
 adicionarOuvintes();
