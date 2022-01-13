0ae19375d9248c9f57ce3654986f85bc9eca7b84
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-13 03:53:58 -0300

    exercicios/7.1 | Parte 1 exercício 2 bônus feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -2,35 +2,48 @@ const LINHA = '-----------------------------------------------------------------
 
 // Exercício 1 -------------------------------------------------------------------------
 console.log(`${LINHA}\nExercício 1\n`);
-
-const testingScope = escopo => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
+{
+  const testingScope = escopo => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
   }
+  testingScope(true);
 }
-testingScope(true);
 
 // Exercício 2 -------------------------------------------------------------------------
 console.log(`${LINHA}\nExercício 2\n`);
+{
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-const ordenarArray = array => {
-  for (let i = 0; i < array.length; i += 1) {
-    for (let j = 0; j < array.length - i; j += 1) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+  const ordenarArray = array => {
+    for (let i = 0; i < array.length; i += 1) {
+      for (let j = 0; j < array.length - i; j += 1) {
+        if (array[j] > array[j + 1]) {
+          const temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
       }
     }
   }
-}
-ordenarArray(oddsAndEvens);
+  ordenarArray(oddsAndEvens);
 
-console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+}
+
+// Exercício 2 Bônus -------------------------------------------------------------------
+console.log(`${LINHA}\nExercício 2 Bônus\n`);
+{
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  const ordenarArray2 = array => array.sort((a, b) => a - b);
+  ordenarArray2(oddsAndEvens);
+
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+}
