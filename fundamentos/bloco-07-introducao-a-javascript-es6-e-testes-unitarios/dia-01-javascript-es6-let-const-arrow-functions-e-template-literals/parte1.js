52ab33ebb1c8492e73381c8b4022a11fed1c7b64
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-13 03:40:12 -0300

    exercicios/7.1 | Parte 1 exercício 2 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/parte1.js
@@ -1,4 +1,7 @@
-// Exercício 1
+const LINHA = '-----------------------------------------------------------------------------'
+
+// Exercício 1 -------------------------------------------------------------------------
+console.log(`${LINHA}\nExercício 1\n`);
 
 const testingScope = escopo => {
   if (escopo === true) {
@@ -10,5 +13,24 @@ const testingScope = escopo => {
     console.log(elseScope);
   }
 }
-
 testingScope(true);
+
+// Exercício 2 -------------------------------------------------------------------------
+console.log(`${LINHA}\nExercício 2\n`);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const ordenarArray = array => {
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length - i; j += 1) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+ordenarArray(oddsAndEvens);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
