3ac11e2298bbc986e56b2b789b6a12ffd45801b2
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-14 01:10:12 -0300

    exercicios/7.2 | Parte 3 exercício 1 feito.

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte3/script.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1 ----------------------------------------------------------------
+console.log('\nExercício 1 -------------------------------------------------\n');
+
+const adicionarPar = (objeto, chave, valor) => { objeto[chave] = valor; };
+adicionarPar(lesson2, 'turno', 'noite');
+console.log(lesson2);
