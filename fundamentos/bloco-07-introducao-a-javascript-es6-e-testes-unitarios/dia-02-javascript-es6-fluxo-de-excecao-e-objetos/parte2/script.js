9277d32e727d42083c4f6f9b153e5d56ae8da183
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-14 00:51:35 -0300

    exercicios/7.2 | Parte 2 exercício 2 feito.

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte2/script.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/parte2/script.js
@@ -34,15 +34,42 @@ const order = {
   },
 };
 
+// Exercício 1 -------------------------------------------------------------------------
+console.log('\nExercício 1: ----------------------------------------------\n');
+
 const customerInfo = (order) => {
   console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
 }
 
 customerInfo(order);
 
-const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+// Exercício 2 -------------------------------------------------------------------------
+console.log('\nExercício 2: ----------------------------------------------\n');
 
+const obterPedidos = (order) => {
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = Object.keys(order.order.drinks).map(drink => order.order.drinks[drink].type);
+  let pedidos = [pizzas, drinks].flat().join(', ');
+
+  const lastIndex = pedidos.lastIndexOf(',');
+  if (lastIndex !== -1) {
+    pedidos = `${pedidos.substring(0, lastIndex)} e${pedidos.substring(lastIndex + 1)}`;
+  }
+
+  return pedidos;
 }
+
+const orderModifier = (order) => {
+  let { total } = order.payment;
+
+  total = total.toString().split('.');
+  total[1] = `${(total[1] !== undefined) ? total[1] : ''}00`.slice(0, 2);
+  total = total.join(',');
+
+  console.log(`Olá ${order.name}, o total do seu pedido de ${obterPedidos(order)} é R$ ${total}.`);
+}
+
+order.name = 'Luiz Silva';
+order.payment.total = 50;
 
 orderModifier(order);
