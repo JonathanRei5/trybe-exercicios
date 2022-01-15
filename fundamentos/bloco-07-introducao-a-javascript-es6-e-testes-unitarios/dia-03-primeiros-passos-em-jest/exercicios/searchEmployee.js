f431b8ac76d0faa18b151e0c4d5f5e47fb51f8fc
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-15 15:28:13 -0300

    exercicios/7.3 | Exercício bônus feito.

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/exercicios/searchEmployee.js
@@ -0,0 +1,56 @@
+// Dados
+const professionalBoard = [
+  {
+    id: '0000-0',
+    firstName: 'Teste',
+    lastName: 'Etset',
+    specialities: ['Todas'],
+  },
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  const ids = professionalBoard.map(funcionario => funcionario.id);
+  const indexID = ids.indexOf(id);
+  if (indexID === -1) return 'ID não identificada';
+  const funcionario = professionalBoard[indexID];
+  return (funcionario[detail] === undefined) ? 'Informação indisponível' : funcionario[detail];
+};
+
+module.exports = searchEmployee;
