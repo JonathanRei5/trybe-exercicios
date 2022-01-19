0ac9846f8379f395f5ea87be3455ad05375155c4
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-01-19 01:15:46 -0300

    exercicios/8.1 | Funções de dano removidas dos objetos

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/bonus.js
@@ -2,55 +2,55 @@ const mage = {
   healthPoints: 130,
   intelligence: 45,
   mana: 125,
-  damage: function () {
-    const mageInfo = {
-      mana: 0,
-      damage: 0,
-    };
-
-    if (this.mana < 15) {
-      mageInfo.damage = 'Não possui mana suficiente';
-    } else {
-      mageInfo.mana = 15;
-      const maxDamage = this.intelligence * 2;
-      mageInfo.damage = Math.round(Math.random() * (maxDamage - this.intelligence));
-      mageInfo.damage += this.intelligence;
-
-      this.mana -= 15;
-    }
-
-    return mageInfo;
-  },
+  damage: undefined,
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: function () {
-    const maxDamage = this.strength * this.weaponDmg;
-    return Math.round(Math.random() * (maxDamage - this.strength)) + this.strength
-  },
+  damage: undefined,
 };
 
 const dragon = {
   healthPoints: 350,
   strength: 50,
-  damage: function () {
-    return Math.round(Math.random() * (this.strength - 15)) + 15
-  },
+  damage: undefined,
 };
 
 const battleMembers = { mage, warrior, dragon };
 
-console.log('Dragon damage:', dragon.damage());
-console.log('Warrior damage:', warrior.damage());
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
-console.log('Mage damage:', mage.damage(), ' | Mana = ', mage.mana);
+function mageDamage() {
+  const mageInfo = {
+    mana: 0,
+    damage: 0,
+  };
+
+  if (mage.mana < 15) {
+    mageInfo.damage = 'Não possui mana suficiente';
+  } else {
+    mageInfo.mana = 15;
+    const maxDamage = mage.intelligence * 2;
+    mageInfo.damage = Math.round(Math.random() * (maxDamage - mage.intelligence));
+    mageInfo.damage += mage.intelligence;
+  }
+
+  return mageInfo;
+}
+
+function warriorDamage() {
+  const maxDamage = warrior.strength * warrior.weaponDmg;
+  return Math.round(Math.random() * (maxDamage - warrior.strength)) + warrior.strength
+}
+
+function dragonDamage() {
+  return Math.round(Math.random() * (dragon.strength - 15)) + 15
+}
+
+console.log(dragonDamage());
+console.log(warriorDamage());
+console.log(mageDamage());
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+};
