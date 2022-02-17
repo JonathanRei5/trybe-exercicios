6e4a29a492e6aa9debb9ce279da8a08b00aefbc5
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-02-17 02:01:36 -0300

    exercicios/10.1 | Exercício 2 feito.

--- modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/hello-world-no-react/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/hello-world-no-react/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+const tasks = [
+  'Estudar',
+  'Fazer Exercícios',
+  'Fazer Projetos'
+];
+
+const Task = (task) => {
+  return (
+    <li>{task.value}</li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {tasks.map((task, index) => <Task key={index} value={task}/>)}
+    </ul>
   );
 }
 
