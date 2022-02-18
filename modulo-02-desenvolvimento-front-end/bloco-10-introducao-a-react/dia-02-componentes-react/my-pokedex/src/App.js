8477db884e8328d76b8a23f1d4a11bd65fb826d1
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-02-18 03:22:29 -0300

    exercicios/10.2 | Fiz algumas correções.
    Adicionei validações utilizando PropTypes, corrigi o link para mais informação do pokemon e adicionei o componente Pokedex(não finalizado).

--- modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './css/App.css';
 import Pokemon from './Pokemon'
 import pokemons from './data'
 
--- modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
+++ modulo-02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my-pokedex/src/Pokemon.js
@@ -1,25 +1,41 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import './css/Pokemon.css'
 
 class Pokemon extends React.Component {
   render() {
-    const info = this.props.pokemon;
-    const { value: weight, measurementUnit: unit } = info.averageWeight;
+    const { name, type, moreInfo, image } = this.props.pokemon;
+    const { value: weight, measurementUnit: unit } = this.props.pokemon.averageWeight;
 
     return (
       <li className="pokemon">
         <div className="info-container">
-          <span>{info.name}</span>
-          <span>{info.type}</span>
+          <span>{name}</span>
+          <span>{type}</span>
           <span>Peso médio {weight} {unit}</span>
-          <span><a className="more-info" href={info.moreInfo} target="_blank">Mais Informação</a></span>
+          <span>
+            <a className="more-info" href={moreInfo} target="_blank" rel="noreferrer">Mais Informação</a>
+          </span>
         </div>
         <div className="image-container">
-          <img src={info.image} alt={`Imagem do ${info.name} de frente.`} />
+          <img src={image} alt={`Imagem do ${name} de frente.`} />
         </div>
       </li>
     )
   }
+}
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    moreInfo: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    averageWeight: PropTypes.exact({
+      value: PropTypes.number.isRequired,
+      measurementUnit: PropTypes.string.isRequired
+    }).isRequired
+  })
 }
 
 export default Pokemon;
