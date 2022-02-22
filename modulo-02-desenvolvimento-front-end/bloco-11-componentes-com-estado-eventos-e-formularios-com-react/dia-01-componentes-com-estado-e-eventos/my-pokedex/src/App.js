a128d7251896dd2447165d027f81f1dc96e38f87
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-02-22 03:07:01 -0300

    exercicios/11.1 | Exercício 2 feito.

--- modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/my-pokedex/src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
+import Filter from './components/Filter';
+import pokemons from './data';
 import './css/App.css';
-import Pokedex from './components/Pokedex'
-import pokemons from './data'
 
 function App() {
   return (
     <div className="pokedex-app">
       <h1>Pokedex</h1>
-      <Pokedex pokemons={pokemons} />
+      <Filter pokemons={pokemons} />
     </div>
   );
 }
