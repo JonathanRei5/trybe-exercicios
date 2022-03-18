b08f0a5cfcc40d4a7ed038561232d1f51e869a6c
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-18 02:00:10 -0300

    exercicios/14.2 | Exercício 3 feito.

--- modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/parte-1/exercicios/service.test.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/parte-1/exercicios/service.test.js
@@ -1,23 +1,40 @@
 const func = require('./service.js');
 
 describe('Exercícios bloco 14 dia 2:', () => {
-  test('Verifica se a função randomNumber é chamada, qual seu retorno e quantas vezes é chamada',
+  test('Verifica se a função randomNumber está definida.',
     () => {
       expect(func.randomNumber).toBeDefined();
+    })
+
+  test('Verifica se a função randomNumber é chamada, qual seu retorno e quantas vezes é chamada.',
+    () => {
       func.randomNumber = jest.fn().mockReturnValue(10);
       expect(func.randomNumber()).toBe(10);
       expect(func.randomNumber).toHaveBeenCalledTimes(1);
     })
 
-    test('Verifica se a função randomNumber é chamada e a nova implementação de divisão é aplicada',
+  test('Verifica se a função randomNumber é chamada e a nova implementação de divisão é aplicada.',
     () => {
-      expect(func.randomNumber).toBeDefined();
-      func.randomNumber = jest.fn().mockImplementationOnce((numA, numB) => numA/numB);
-      expect(func.randomNumber(10,2)).toBe(5);
+      func.randomNumber = jest.fn().mockImplementationOnce((numA, numB) => numA / numB);
+      expect(func.randomNumber(10, 2)).toBe(5);
       expect(func.randomNumber).toHaveBeenCalledTimes(1);
-      expect(func.randomNumber(10,2)).not.toBe(5);
+      expect(func.randomNumber(10, 2)).not.toBe(5);
       expect(func.randomNumber).toHaveBeenCalledTimes(2);
     })
 
+  test('Faz novas inplementações na função randomNumber e verifica seu retorno.',
+    () => {
+      func.randomNumber = jest.fn().mockImplementation(
+        (numA, numB, numC) => numA * numB * numC
+      );
+      expect(func.randomNumber(10, 2, 5)).toBe(100);
+      expect(func.randomNumber).toHaveBeenCalledTimes(1);
+      func.randomNumber.mockReset();
+      func.randomNumber.mockImplementation(
+        (numA) => numA * 2
+      );
+      expect(func.randomNumber(10)).toBe(20);
+      expect(func.randomNumber).toHaveBeenCalledTimes(1);
+    })
 
 })
