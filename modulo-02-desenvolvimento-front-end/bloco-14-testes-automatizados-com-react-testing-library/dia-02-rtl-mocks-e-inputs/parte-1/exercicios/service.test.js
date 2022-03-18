902929fe229e2c50944f9f8d5ff65196e9272b99
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-18 01:45:40 -0300

    exercicios/14.2 | Exercício 2 feito.

--- modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/parte-1/exercicios/service.test.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/parte-1/exercicios/service.test.js
@@ -8,4 +8,16 @@ describe('Exercícios bloco 14 dia 2:', () => {
       expect(func.randomNumber()).toBe(10);
       expect(func.randomNumber).toHaveBeenCalledTimes(1);
     })
+
+    test('Verifica se a função randomNumber é chamada e a nova implementação de divisão é aplicada',
+    () => {
+      expect(func.randomNumber).toBeDefined();
+      func.randomNumber = jest.fn().mockImplementationOnce((numA, numB) => numA/numB);
+      expect(func.randomNumber(10,2)).toBe(5);
+      expect(func.randomNumber).toHaveBeenCalledTimes(1);
+      expect(func.randomNumber(10,2)).not.toBe(5);
+      expect(func.randomNumber).toHaveBeenCalledTimes(2);
+    })
+
+
 })
