81d3f3b583665d50240b5800e038d08d908f107c
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-22 02:17:07 -0300

    exercicios/14.3 | Página Projetos com link para o github feita e testes realizados.

--- modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/pages/Projects.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/pages/Projects.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Projects extends React.Component {
   render() {
     return (
-      <h1>Projetos</h1>
+      <main>
+        <h1>Projetos</h1>
+        <a
+          href="https://github.com/JonathanRei5"
+          rel="noopener noreferrer"
+          target="_blanck"
+        >GitHub</a>
+      </main>
     );
   };
 }
---
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/tests/Projects.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Projects from '../pages/Projects'
+
+describe('Verifica a página Projetos', () => {
+  test('Verifica se contem um link para o GitHub.', () => {
+    render(<Projects />);
+    const gitHubLink = screen.getByRole('link', { name: /GitHub/i });
+    expect(gitHubLink).toHaveAttribute('href', 'https://github.com/JonathanRei5');
+    userEvent.click(gitHubLink);
+  });
+});
