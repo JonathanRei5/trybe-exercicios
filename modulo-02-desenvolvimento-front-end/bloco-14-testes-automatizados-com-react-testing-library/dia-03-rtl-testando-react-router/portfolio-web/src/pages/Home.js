67ea101f02377210df38df2b65dfa5060bfb5e64
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-22 00:19:18 -0300

    exercicios/14.3 | Página com nome e foto feita e testes realizados.

--- modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/pages/Home.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import profilePicture from '../image/blank-profile-picture.png';
+import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <h1>Início</h1>
+      <main className='Home'>
+        <h1>Início</h1>
+        <img
+          className="profilePicture"
+          src={profilePicture}
+          alt="Foto de perfil genérica"
+        />
+        <h3>Descrição</h3>
+        <p>Meu nome é Jonathan sou estudante de desenvolvimento web na Trybe, moro em São Paulo - SP, Brasil.</p>
+      </main>
     );
   };
 }
