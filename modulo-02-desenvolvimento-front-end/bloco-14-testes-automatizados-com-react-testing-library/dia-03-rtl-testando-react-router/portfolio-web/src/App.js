86a349540690e0c6092d04e6931f175191950d50
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-03-21 21:02:05 -0300

    exercicios/14.3 | Rota para as páginas criada

--- modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/App.js
+++ modulo-02-desenvolvimento-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-03-rtl-testando-react-router/portfolio-web/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </div>
   );
 }
