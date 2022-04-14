72b7a2be304766fd70958ebadd93edda58fda12d
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-04-14 13:54:15 -0300

    exercicios/17.2 | Exercício 2 feito.

--- modulo-02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-02-react-hooks-usestate-e-usecontext/exercicio-2/src/TrafficSignal.jsx
+++ modulo-02-desenvolvimento-front-end/bloco-17-context-api-e-react-hooks/dia-02-react-hooks-usestate-e-usecontext/exercicio-2/src/TrafficSignal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import redSignal from './images/redSignal.jpeg';
 import yellowSignal from './images/yellowSignal.jpeg';
 import greenSignal from './images/greenSignal.jpeg';
@@ -11,28 +11,24 @@ const renderSignal = (signalColor) => {
   return null;
 };
 
-class TrafficSignal extends React.Component {
-  render() {
-    const { signalColor, changeSignal } = this.context;
-    return (
-      <div>
-        <div className="button-container">
-          <button onClick={() => changeSignal('red')} type="button">
-            Red
-          </button>
-          <button onClick={() => changeSignal('yellow')} type="button">
-            Yellow
-          </button>
-          <button onClick={() => changeSignal('green')} type="button">
-            Green
-          </button>
-        </div>
-        <img className="signal" src={renderSignal(signalColor)} alt="" />
+function TrafficSignal() {
+  const { signalColor, changeSignal } = useContext(trafficSignalContext);
+  return (
+    <div>
+      <div className="button-container">
+        <button onClick={() => changeSignal('red')} type="button">
+          Red
+        </button>
+        <button onClick={() => changeSignal('yellow')} type="button">
+          Yellow
+        </button>
+        <button onClick={() => changeSignal('green')} type="button">
+          Green
+        </button>
       </div>
-    );
-  };
+      <img className="signal" src={renderSignal(signalColor)} alt="" />
+    </div>
+  );
 };
-
-TrafficSignal.contextType = trafficSignalContext;
 
 export default TrafficSignal;
