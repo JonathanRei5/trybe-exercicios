76eec4f8dbc813f3e25d1b2abc1f60c33c1a3450
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-05-16 04:21:48 -0300

    exercicios/19.1 | Parâmetro -e adicionado no comando echo

--- modulo-03-desenvolvimento-back-end/bloco-19-docker-utilizando-containers/dia-01-utilizando-containers-docker/exercicios.sh
+++ modulo-03-desenvolvimento-back-end/bloco-19-docker-utilizando-containers/dia-01-utilizando-containers-docker/exercicios.sh
@@ -6,50 +6,50 @@ echo "Link da Imagem da Distribuição Linux Debian: https://hub.docker.com/_/de
 sleep $wait
 
 # Exercício 2
-echo "\nBaixando a imagem localmente"
+echo -e "\nBaixando a imagem localmente"
 docker pull debian
 sleep $wait
 
 # Exercício 3
-echo "\nBaixando a versão reduzida da imagem"
+echo -e "\nBaixando a versão reduzida da imagem"
 docker pull debian:stable-slim
 sleep $wait
 
 Exercícios 4, 5 e 6
-echo "\nCriando e executando um contêiner no modo interativo"
+echo -e "\nCriando e executando um contêiner no modo interativo"
 echo "O comando \"cat /etc/*-release\" retorna os dados da distribuição Debian"
 echo "O comando \"exit\" encerra a execução da distribuição Debian"
 docker container run -it --name debian -it debian:stable-slim
 sleep $wait
 
 # Exercício 7
-echo "\nVerificando a lista de contêiners"
+echo -e "\nVerificando a lista de contêiners"
 docker container ls -a
 sleep $wait
 
 # Exercício 8
-echo "\nIniciando o mesmo contêiner"
+echo -e "\nIniciando o mesmo contêiner"
 docker container start debian
 echo "Verificando se ele está ativo na lista de contêiners"
 docker container ls -a
 sleep $wait
 
 # Exercícios 9, 10 e 11
-echo "\nRetomando o contêiner"
+echo -e "\nRetomando o contêiner"
 echo "O comando \"cat /etc/debian_version\" retorna a versão atual do sistema do contêiner"
 echo "O comando \"exit\" encerra o terminal"
 docker container exec -it debian bash
 sleep $wait
 
 # Exercício 12
-echo "\nParando o contêiner para remove-lo"
+echo -e "\nParando o contêiner para remove-lo"
 docker container stop debian
 echo "Removendo o contêiner"
 docker container rm debian
 sleep $wait
 
 # Exercícios 13 e 14
-echo "\nCriando e rodando a imagem andrius/ascii-patrol"
+echo -e "\nCriando e rodando a imagem andrius/ascii-patrol"
 echo "Os botões \"Ctrl + C\" encerra o contêiner"
 echo "Iniciando..."
 sleep $waitMore
