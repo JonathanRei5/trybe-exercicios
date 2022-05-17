3d3a1dec88b8349541ef9cfdeea6a3b50cf6c4ab
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-05-17 12:55:15 -0300

    exercicios/19.3 | Exercício 1 feito

---
+++ modulo-03-desenvolvimento-back-end/bloco-19-docker-utilizando-containers/dia-03-orquestrando-containers-com-docker-compose/exercicio-1/exercicio.sh
@@ -0,0 +1,52 @@
+# !/bin/bash
+
+WAIT=2
+
+echo "Criando um arquivo HTML"
+cat missao_trybe_antes.html > htdocs/missao_trybe.html
+sleep $WAIT
+
+if [[ $(docker container ls -a | grep server_missao_trybe) ]]; then
+  echo -e "\nRemovendo o container server_missao_trybe"
+  docker container rm -f server_missao_trybe
+fi
+echo -e "\nCriando container para manter um servidor httpd:2.4 Apache"
+docker container run --name server_missao_trybe -itd -p 4545:80 -v "$PWD"/htdocs:/usr/local/apache2/htdocs/ httpd:2.4
+sleep $WAIT
+
+echo -e "\nLink da página HTML"
+echo "http://localhost:4545/missao_trybe.html"
+if [[ ! $(which xdg-open | grep 'not found') ]]; then
+    xdg-open http://localhost:4545/missao_trybe.html
+fi
+sleep $WAIT
+
+echo -e "\nAlterando o arquivo HTML"
+cat missao_trybe_depois.html > htdocs/missao_trybe.html
+sleep $WAIT
+
+echo -e "\nListando os containers para obtendo o id do container httpd:2.4"
+docker container ls
+
+echo -e "\nVerificando o volume do container no Docker Host"
+docker container inspect server_missao_trybe | egrep Source
+
+echo -e "\nParando o container"
+docker container stop server_missao_trybe
+
+echo -e "\nExcluindo o container"
+docker container rm server_missao_trybe
+
+echo -e "\nVerificando se a pasta permanece no mesmo lugar"
+if [ -d htdocs ]; then
+  echo "A pasta permanece no mesmo lugar"
+  echo "$PWD"/htdocs
+else 
+  echo "A pasta não permanece no mesmo lugar"
+fi
+
+echo -e "\nObtendo o IMAGE ID do servidor"
+docker images -q httpd:2.4
+
+echo -e "\nExcluindo a imagem"
+docker image rm -f $(docker images -q httpd:2.4)
