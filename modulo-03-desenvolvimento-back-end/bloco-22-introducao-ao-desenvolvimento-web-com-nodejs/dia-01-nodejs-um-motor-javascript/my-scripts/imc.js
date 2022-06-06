33b78037d5e4e17405c18c530bb7e76a1780a739
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-06 19:23:55 -0300

    exercicios/22.1 | finaliza exercícios 3-4

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -1,8 +1,11 @@
-const height = Math.random()*2.5;
-const weight = Math.random()*400;
+const { questionFloat } = require('readline-sync');
+
+const height = questionFloat('Qual sua altura? ');
+const weight = questionFloat('Qual seu peso? ');
 
 const imc = weight/height**2;
 
-console.log(`  Peso = ${weight.toFixed(2)} kg`);
+console.log('-----------------------------');
 console.log(`Altura = ${height.toFixed(2)} m`);
+console.log(`  Peso = ${weight.toFixed(2)} kg`);
 console.log(`   IMC = ${imc.toFixed(2)}`);
