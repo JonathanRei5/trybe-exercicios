f6b155ce574f248fb61747b8ed2c42ac9fc8643e
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-15 00:04:29 -0300

    exercicios/22.4 | finaliza exercício 1

--- modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-express-http-com-nodejs/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!');
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
