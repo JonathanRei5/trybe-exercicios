d2d47a1620adbccf33020bbb97f8335b88e04260
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-24 13:47:10 -0300

    exercicios/23.1 | renomeia service/userAPI.js -> services/userService.js

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-01-arquitetura-de-software-camada-de-model/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userAPI = require('./service/userAPI');
+const userService = require('./services/userService');
 
 const PORT = 3001;
 
@@ -9,22 +9,22 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/user', async (_req, res) => {
-  const users = await userAPI.getAll();
+  const users = await userService.getAll();
   res.status(200).json(users);
 });
 
 app.get('/user/:id', async (req, res) => {
   const { id } = req.params;
-  const user = await userAPI.findById(id);
+  const user = await userService.findById(id);
   if (user.error) return res.status(404).json(user);
   res.status(200).json(user)
 });
 
 app.post('/user', async (req, res) => {
   const user = req.body;
-  const { error, message } = userAPI.isValid(user);
+  const { error, message } = userService.isValid(user);
   if (error) return res.status(400).json({ error, message });
-  const userCreated = await userAPI.create(user);
+  const userCreated = await userService.create(user);
   res.status(201).json(userCreated);
 });
 
@@ -32,10 +32,10 @@ app.put('/user/:id', async (req, res) => {
   const user = req.body;
   const { id } = req.params;
 
-  const { error, message } = userAPI.isValid(user);
+  const { error, message } = userService.isValid(user);
   if (error) return res.status(400).json({ error, message });
 
-  const updatedUser = await userAPI.update(id, user);
+  const updatedUser = await userService.update(id, user);
   if (updatedUser.error) return res.status(404).json(updatedUser);
 
   res.status(200).json(updatedUser);
