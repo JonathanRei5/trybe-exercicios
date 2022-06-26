c194d4718893dbcb946657b2d5fc017ce27dab18
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-26 03:10:40 -0300

    exercicios/23.2 | altera função
    altera a função getByCep de cepModel.js para retornar um objeto no
    lugar de um array

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/cepModel.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/cepModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getByCep = async (cep) => {
   const query = 'SELECT * FROM ceps WHERE cep=?';
-  const [result] = await connection.execute(query, [cep]);
+  const [[result]] = await connection.execute(query, [cep]);
   return result;
 };
 
