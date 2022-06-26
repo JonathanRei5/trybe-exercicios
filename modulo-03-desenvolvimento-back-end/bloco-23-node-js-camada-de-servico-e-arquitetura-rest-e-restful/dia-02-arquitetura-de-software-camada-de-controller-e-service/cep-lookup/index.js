da14236ce116da636fc7651b7056119d4a88f5c4
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-26 03:57:08 -0300

    exercicios/23.2 | finaliza exercício 2

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+require('dotenv').config();
+require('express-async-errors');
 
-const PORT = 3000;
+const express = require('express');
+const cepController = require('./controllers/cepController');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/ping', async (_req, res) => {
   res.status(200).json({ "message": "pong!" });
+});
+
+app.get('/cep/:cep', cepController.getByCep);
+
+app.use((err, _req, res, _next) => {
+  const { name, status, error } = err;
+  if (name === 'customError') return res.status(status).json({ error });
+  res.status(500).json({
+    error: { code: 'internalError', message: 'Erro interno do servidor' }
+  });
 });
 
 app.listen(PORT, () => console.log(`Aplicação ouvindo na porta ${PORT}`));
