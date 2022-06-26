63360f99d7a8f7196ec4a527ec688816c232edd0
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-26 19:27:48 -0300

    exercicios/23.2 | adiciona validações dos dados do cep

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepService.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/services/cepService.js
@@ -1,5 +1,15 @@
+const joi = require('joi');
 const cepModel = require('../models/cepModel');
 const customError = require('../errors/customError');
+const validateSchema = require('./validateSchema');
+
+const cepDataSchema = joi.object({
+  cep: joi.string().not().empty().required().pattern(/\d{5}-\d{3}/),
+  logradouro: joi.string().not().empty().required(),
+  bairro: joi.string().not().empty().required(),
+  localidade: joi.string().not().empty().required(),
+  uf: joi.string().not().empty().required(),
+});
 
 const validateCep = (cep) => {
   if (!cep.match(/\d{5}-?\d{3}/g)) {
@@ -7,10 +17,12 @@ const validateCep = (cep) => {
   }
 };
 
+const validateCepData = (cepData) => validateSchema(cepDataSchema, cepData);
+
 const getByCep = async (cep) => {
   const result = await cepModel.getByCep(cep.replace('-', ''));
   if (!result) throw new customError(404, 'notFound', 'CEP não encontrado');
   return result;
 };
 
-module.exports = { validateCep, getByCep };
+module.exports = { validateCep, validateCepData, getByCep };
