75daf226bae86b55cb9a46fbadca5842a368d6ac
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-27 14:47:31 -0300

    exercicios/23.2 | cep-lookup-2 - adiciona função para procurar bairro no banco de dados

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup-2/models/bairroModel.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup-2/models/bairroModel.js
@@ -3,9 +3,14 @@ const connection = require('./connection');
 const add = async (data) => {
   const query = 'INSERT INTO bairros (bairro, localidade, uf) values (?,?,?)';
   const { bairro, localidade, uf } = data;
-  const [{ insertId }] = await connection
-    .execute(query, [bairro, localidade, uf]);
-  return insertId;
+  const [{ insertId }] = await connection.execute(query, [bairro, localidade, uf]);
+  return !insertId;
 };
 
-module.exports = { add };
+const search = async ({ bairro, localidade, uf }) => {
+  const query = 'SELECT id FROM bairros WHERE bairro=? AND localidade=? AND uf=?';
+  const [result] = await connection.execute(query, [bairro, localidade, uf]);
+  return result;
+}
+
+module.exports = { add, search };
