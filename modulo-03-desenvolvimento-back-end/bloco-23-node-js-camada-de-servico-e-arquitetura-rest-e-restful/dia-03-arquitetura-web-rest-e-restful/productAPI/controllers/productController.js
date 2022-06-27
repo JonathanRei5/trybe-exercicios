cdede0667dfd7c2825113093dde9749320dae608
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-27 19:37:23 -0300

    exercicios/23.3 | altera os endpoints e separa as rotas do controlador

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/controllers/productController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/controllers/productController.js
@@ -1,40 +1,37 @@
-const express = require('express');
 const ProductModel = require('../models/productModel');
 
-const router = express.Router();
+module.exports = {
+  async listProducts(_req, res) {
+    const products = await ProductModel.getAll();
 
-router.get('/list-products', async (req, res) => {
-  const products = await ProductModel.getAll();
+    res.send(products);
+  },
 
-  res.send(products);
-});
+  async getProduct(req, res) {
+    const product = await ProductModel.getById(req.params.id);
 
-router.get('/get-by-id/:id', async (req, res) => {
-  const product = await ProductModel.getById(req.params.id);
+    res.send(product);
+  },
 
-  res.send(product);
-});
+  async addProduct(req, res) {
+    const { name, brand } = req.body;
 
-router.post('/add-product', async (req, res) => {
-  const { name, brand } = req.body;
+    const newProduct = await ProductModel.add(name, brand);
 
-  const newProduct = await ProductModel.add(name, brand);
+    res.send(newProduct);
+  },
 
-  res.send(newProduct);
-});
+  async deleteProduct(req, res) {
+    const products = await ProductModel.exclude(req.params.id);
 
-router.post('/delete-product/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
+    res.send(products);
+  },
 
-  res.send(products);
-});
+  async updateProduct(req, res) {
+    const { name, brand } = req.body;
 
-router.post('/update-product/:id', async (req, res) => {
-  const { name, brand } = req.body;
+    const products = await ProductModel.update(req.params.id, name, brand);
 
-  const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.send(products);
-});
-
-module.exports = router;
+    res.send(products);
+  },
+};
--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const productRoute = require('./routes/productRoute');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,7 +10,7 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/products', require('./controllers/productController'));
+app.use('/products', productRoute);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
