c0b575c0c62589bc0c0a7f3e9b493d8fd7078873
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-27 03:28:19 -0300

    exercicios/23.2 | cep-lookup-2 - altera cepModel de acordo com o novo banco de dados

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup-2/models/cepModel.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-02-arquitetura-de-software-camada-de-controller-e-service/cep-lookup-2/models/cepModel.js
@@ -1,18 +1,16 @@
 const connection = require('./connection');
 
 const getByCep = async (cep) => {
-  const query = 'SELECT * FROM ceps WHERE cep=?';
+  const query = 'SELECT cep, logradouro FROM ceps WHERE cep=?';
   const [[result]] = await connection.execute(query, [cep]);
   return result;
 };
 
 const add = async (cepData) => {
-  const query = `
-    INSERT INTO ceps (cep, logradouro, bairro, localidade, uf) values (?,?,?,?,?)
-  `;
-  const { cep, logradouro, bairro, localidade, uf } = cepData;
+  const query = 'INSERT INTO ceps (cep, logradouro) values (?,?)';
+  const { cep, logradouro } = cepData;
   const [{ inserId }] = await connection
-    .execute(query, [cep, logradouro, bairro, localidade, uf]);
+    .execute(query, [cep, logradouro]);
   return inserId;
 };
 
