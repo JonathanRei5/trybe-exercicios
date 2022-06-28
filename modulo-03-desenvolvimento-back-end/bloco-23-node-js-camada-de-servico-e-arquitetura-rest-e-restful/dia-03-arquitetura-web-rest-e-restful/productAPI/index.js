f3f48ceca262f6d72167647614989670962a947e
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-28 02:55:55 -0300

    exercicios/23.3 | adiciona middleware de tratamento de erros

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+require('express-async-errors');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const productRoute = require('./routes/productRoute');
@@ -10,6 +12,14 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/products', productRoute);
+
+app.use((err, _req, res, _next) => {
+  const { name, status, error } = err;
+  if (name === 'customError') return res.status(status).json({ error });
+  res.status(500).json({
+    error: { code: 'internalError', message: 'Erro interno do servidor' }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
