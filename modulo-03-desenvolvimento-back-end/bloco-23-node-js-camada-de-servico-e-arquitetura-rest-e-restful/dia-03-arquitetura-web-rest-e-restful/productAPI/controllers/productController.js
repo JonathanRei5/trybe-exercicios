4a22ef3eb4b439e0bdefe56d1a510285fd92d722
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-28 04:06:24 -0300

    exercicios/23.3 | adiciona validações

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/controllers/productController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/controllers/productController.js
@@ -7,21 +7,26 @@ module.exports = {
   },
 
   async getProduct(req, res) {
+    await productService.checkIfExists(req.params.id);
     const product = await productService.getProduct(req.params.id);
     res.status(200).json(product);
   },
 
   async addProduct(req, res) {
+    productService.validadeProduct(req.body);
     const product = await productService.addProduct(req.body);
     res.status(201).json(product);
   },
 
   async deleteProduct(req, res) {
+    await productService.checkIfExists(req.params.id);
     await productService.deleteProduct(req.params.id);
     res.status(204).end();
   },
 
   async updateProduct(req, res) {
+    productService.validadeProduct(req.body);
+    await productService.checkIfExists(req.params.id);
     const product = await productService.updateProduct(req.params.id, req.body);
     res.status(200).json(product);
   },
--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/services/productService.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/productAPI/services/productService.js
@@ -1,6 +1,22 @@
 const productModel = require('../models/productModel');
+const joi = require('joi');
+const customError = require('../errors/customError');
+const validateSchema = require('./validateSchema');
 
 module.exports = {
+  validadeProduct(product) {
+    const productSchema = joi.object({
+      name: joi.string().required().not().empty(),
+      brand: joi.string().required().not().empty(),
+    });
+    validateSchema(productSchema, product);
+  },
+
+  async checkIfExists(id) {
+    const product = await productModel.getProduct(id);
+    if (!product) throw new customError(404, 'notFound', 'Produto não encontrado');
+  },
+
   async listProducts() {
     const products = await productModel.listProducts();
     return products;
