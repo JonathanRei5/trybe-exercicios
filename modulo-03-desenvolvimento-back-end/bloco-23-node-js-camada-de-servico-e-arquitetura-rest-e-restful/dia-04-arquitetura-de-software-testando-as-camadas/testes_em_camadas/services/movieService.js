cffd19b736c968cdd8a099f4f02ecd48bd68d405
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-29 04:30:14 -0300

    exercicios/23.4 | cria função get da camada service

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/services/movieService.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/services/movieService.js
@@ -1,5 +1,7 @@
 const MoviesModel = require('../models/movieModel');
 
+const validateId = (id) => (typeof id === 'number' && id > 0);
+
 const validateTitle = (title) => (title && typeof title === 'string');
 
 const validateReleaseYear = (releaseYear) => (releaseYear && typeof releaseYear === 'number');
@@ -10,25 +12,23 @@ const isValid = (title, directedBy, releaseYear) => {
   const isValidTitle = validateTitle(title);
   const isValidReleaseYear = validateReleaseYear(releaseYear);
   const isValidDirectedBy = validateDirectedBy(directedBy);
-
   if (!isValidTitle || !isValidReleaseYear || !isValidDirectedBy) return false;
-
   return true;
 };
 
 const create = async ({ title, directedBy, releaseYear }) => {
   const isMovieValid = isValid(title, directedBy, releaseYear);
-
   if (!isMovieValid) return false;
+  const { id } = await MoviesModel.create({ title, directedBy, releaseYear });
+  return { id };
+};
 
-  const { id } = await MoviesModel
-    .create({ title, directedBy, releaseYear });
-
-  return {
-    id,
-  };
+const get = async (id) => {
+  if (!validateId(id)) return undefined;
+  return await MoviesModel.get(id);
 };
 
 module.exports = {
   create,
+  get,
 };
