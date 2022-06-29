fb42edebc28008d3d4773184b92b2349ab8d5b7d
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-29 01:05:27 -0300

    exercicios/23.4 | configura variáveis de ambiente de acesso ao banco de dados

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/models/connection.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/models/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'senha_muito_segura',
-  database: 'model_example'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'senha_muito_segura',
+  database: process.env.DB_NAME || 'model_example'
 });
 
 module.exports = connection;
--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/models/movieModel.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/models/movieModel.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const create = async ({ title, directedBy, releaseYear }) => {
   const [result] = await connection
     .execute(
-      'INSERT INTO model_example.movies (title, directed_by, release_year) VALUES (?, ?, ?)',
+      'INSERT INTO movies (title, directed_by, release_year) VALUES (?, ?, ?)',
       [title, directedBy, releaseYear],
     );
 
