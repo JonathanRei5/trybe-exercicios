b61fdf8bd57e3a84c670fe6b7d042cdfbb29076c
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-06-29 13:40:03 -0300

    exercicios/23.4 | cria função get da camada controller

--- modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/controllers/movieController.js
+++ modulo-03-desenvolvimento-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dia-04-arquitetura-de-software-testando-as-camadas/testes_em_camadas/controllers/movieController.js
@@ -2,21 +2,19 @@ const MoviesService = require('../services/movieService');
 
 const create = async (req, res) => {
   const { title, directedBy, releaseYear } = req.body;
+  const movie = await MoviesService.create({ title, directedBy, releaseYear });
+  if (!movie) return res.status(400).send('Dados inválidos');
+  res.status(201).send('Filme criado com sucesso!');
+};
 
-  const movie = await MoviesService
-    .create({ title, directedBy, releaseYear });
-
-  if (!movie) {
-    return res
-      .status(400)
-      .send('Dados inválidos');
-  }
-
-  res
-    .status(201)
-    .send('Filme criado com sucesso!');
+const get = async (req, res) => {
+  const { id } = req.params;
+  const movie = await MoviesService.get(Number(id));
+  if (!movie) return res.status(404).send('Filme não encontrado');
+  res.status(200).json(movie);
 };
 
 module.exports = {
   create,
+  get,
 };
