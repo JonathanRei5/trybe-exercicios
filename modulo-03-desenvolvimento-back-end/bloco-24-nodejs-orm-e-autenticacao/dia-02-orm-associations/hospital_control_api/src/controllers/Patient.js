cf51f64121973b9c3b93212f9faacbba74092843
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-11 03:39:31 -0300

    exercicios/24.2 | refatora método
    refatora o método Patient.listPatients para listar os pacientes de
    acordo com o id do plano dos mesmos

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/controllers/Patient.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/controllers/Patient.js
@@ -3,9 +3,10 @@ const Patient = require('../services/Patient');
 module.exports = {
   listPatients: async (req, res) => {
     const { plan, surgeries } = req.query;
+    const { filter } = req.body;
     const withPlan = plan === 'true';
     const withSurgeries = surgeries === 'true';
-    const patients = await Patient.listPatients(withPlan, withSurgeries);
+    const patients = await Patient.listPatients(withPlan, withSurgeries, filter);
     res.status(200).json(patients);
   },
 };
--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/services/Patient.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/services/Patient.js
@@ -1,18 +1,21 @@
 const { Patient, Plan, Surgery } = require('../database/models');
 
 module.exports = {
-  listPatients: async (withPlan, withSurgeries) => {
+  listPatients: async (withPlan, withSurgeries, { planId } = {}) => {
     const include = [];
+    const where = {};
     if (withPlan) include.push({ model: Plan, as: 'plan' });
     if (withSurgeries) include.push({
       model: Surgery,
       as: 'surgeries',
       through: { attributes: [] },
     });
+    if (planId) where.planId = planId;
 
     const patients = await Patient.findAll({
       attributes: { exclude: ['plan_id', 'planId'] },
       include,
+      where,
     });
 
     return patients;
