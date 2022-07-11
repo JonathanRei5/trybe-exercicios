4550788f1fb64beb91c435ded8e4c3e8c14f8e93
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-11 13:13:08 -0300

    exercicios/24.2 | refatora método
    refatora o método Patient.listPatients para ser capaz de ocultar o nome
    do médico responsável pela cirurgia

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/controllers/Patient.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/controllers/Patient.js
@@ -4,9 +4,10 @@ module.exports = {
   listPatients: async (req, res) => {
     const { plan, surgeries } = req.query;
     const { filter } = req.body;
+    const { options } = req.body;
     const withPlan = plan === 'true';
     const withSurgeries = surgeries === 'true';
-    const patients = await Patient.listPatients(withPlan, withSurgeries, filter);
+    const patients = await Patient.listPatients(withPlan, withSurgeries, filter, options);
     res.status(200).json(patients);
   },
 };
--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/services/Patient.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-02-orm-associations/hospital_control_api/src/services/Patient.js
@@ -1,16 +1,19 @@
 const { Patient, Plan, Surgery } = require('../database/models');
 
 module.exports = {
-  listPatients: async (withPlan, withSurgeries, { planId } = {}) => {
+  listPatients: async (withPlan, withSurgeries, { planId } = {}, { hideDoctor } = {}) => {
     const include = [];
     const where = {};
+    const exclude = [];
+    if (planId) where.planId = planId;
+    if (hideDoctor) exclude.push('doctor');
     if (withPlan) include.push({ model: Plan, as: 'plan' });
     if (withSurgeries) include.push({
       model: Surgery,
       as: 'surgeries',
       through: { attributes: [] },
+      attributes: { exclude },
     });
-    if (planId) where.planId = planId;
 
     const patients = await Patient.findAll({
       attributes: { exclude: ['plan_id', 'planId'] },
