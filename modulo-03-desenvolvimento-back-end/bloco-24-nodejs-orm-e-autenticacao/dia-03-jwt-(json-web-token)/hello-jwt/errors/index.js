21b09d9b48a81f66ed4c2d87f62e1e4a2dbe12e5
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-12 03:01:13 -0300

    exercicios/24.3 | adiciona erro forbidden

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/errors/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/errors/index.js
@@ -4,4 +4,7 @@ module.exports = {
   unauthorized: (message) => {
     return new CustomError(401, message || 'Usuário não existe ou senha inválida');
   },
+  forbidden: (message) => {
+    return new CustomError(403, message || 'Acesso restrito');
+  },
 };
