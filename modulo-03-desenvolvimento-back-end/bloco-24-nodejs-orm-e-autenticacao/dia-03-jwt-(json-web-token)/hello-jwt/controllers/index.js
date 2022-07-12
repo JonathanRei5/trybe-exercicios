087f75ac51850583c71505f9501881fb395e2fae
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-12 03:04:04 -0300

    exercicios/24.3 | adiciona função
    adiciona a função topSecretController.getSecret

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/controllers/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/controllers/index.js
@@ -1,9 +1,11 @@
 const ping = require('./ping');
 const authController = require('./authController');
 const usersController = require('./userController');
+const topSecretController = require('./topSecretController');
 
 module.exports = {
   ping,
   authController,
   usersController,
+  topSecretController,
 };
