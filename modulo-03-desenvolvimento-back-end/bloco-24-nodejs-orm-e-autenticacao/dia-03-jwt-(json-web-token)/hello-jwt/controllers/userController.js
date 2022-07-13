271bd5c04d63a5b91d693e2da212423822aaf0b8
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-13 01:20:54 -0300

    exercicios/24.3 | adiciona funções
    adiciona as funções:
    - userService.usernameAlreadyExists
    - userService.create
    - userController.create

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/controllers/userController.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/controllers/userController.js
@@ -1,6 +1,19 @@
+const { userService } = require("../services");
+const { createToken } = require("../services/jwtService");
+const { usernameAlreadyExists } = require("../services/userService");
+
 module.exports = {
   getUserAuthorization: (req, res) => {
     const { dataValues: user } = req;
     res.status(200).json(user);
+  },
+
+  create: async (req, res) => {
+    const { body } = req;
+    await usernameAlreadyExists(body.username);
+    const createdUser = userService.create(body);
+    const { username, admin } = createdUser;
+    const token = createToken({ username, admin });
+    res.status(201).json({ token });
   },
 };
--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/services/index.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-03-jwt-(json-web-token)/hello-jwt/services/index.js
@@ -1,7 +1,9 @@
 const authService = require('./authService');
 const jwtService = require('./jwtService');
+const userService = require('./userService');
 
 module.exports = {
   authService,
   jwtService,
+  userService,
 };
