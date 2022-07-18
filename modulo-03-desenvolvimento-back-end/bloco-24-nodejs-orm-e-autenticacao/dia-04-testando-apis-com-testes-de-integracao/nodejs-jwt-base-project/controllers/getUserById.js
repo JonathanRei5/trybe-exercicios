cde462c8283f90ad3cd9e27df68560b1a0b5612d
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-18 19:43:26 -0300

    exercicios/24.4 | altera o controlador getUserById
    altera o controlador getUserById para não retornar os dados de outro
    usuário

--- modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/controllers/getUserById.js
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/controllers/getUserById.js
@@ -3,9 +3,11 @@ const { User } = require('../models');
 module.exports = async (req, res) => {
   try {
     const { id } = req.params;
+    const { userId } = req.tokenPayload;
     const user = await User.findByPk(id);
 
     if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+    if (user.id !== userId) return res.status(401).json({ message: 'Acesso negado' });
 
     res.status(200).json(user);
   } catch (err) {
