69cea94140153764977c5e2830a3b3db78caa7d0
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-18 18:14:55 -0300

    exercicios/24.4 | adiciona endpoint
    adiciona o endpoint GET /api/users/:userId

---
+++ modulo-03-desenvolvimento-back-end/bloco-24-nodejs-orm-e-autenticacao/dia-04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/controllers/getUserById.js
@@ -0,0 +1,16 @@
+const { User } = require('../models');
+
+module.exports = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'Erro ao buscar usuários no banco', error: err.message });
+  }
+};
