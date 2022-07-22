11b5308184eae1d03e55226d4824c4e6e925323b
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-22 00:16:29 -0300

    exercicios/25.1 | altera o endpoint GET /
    usa a mensagem na variável de ambiente

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Está vivo!!!');
+  res.send(process.env.RESPONSE_MESSAGE);
 });
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
